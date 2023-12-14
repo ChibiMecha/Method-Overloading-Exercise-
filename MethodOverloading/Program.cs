9a057f04f188ea816e26aabc3c3b5d45358f6f93
Vernon Hudson <vhudson@crimson.ua.edu>
2023-12-14 16:33:13 -0600

    Overloaded methods(FAVORITE EXERCISE SO FAR). included if and else statement to satisfiy conditions of method and called methods to ensure usability.

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Add(8,8));
+            Console.WriteLine(Add(8.8m,777.7m));
+            Console.WriteLine(Add(0.2m,0.4m,true));
         }
+    public static int Add(int x, int y)
+    {
+        return x + y;
+    }
+
+    public static decimal Add(decimal x, decimal y)
+    {
+        return x + y;
+    }
+    
+    public static string Add(decimal x, decimal y, bool notRich)
+    {
+        if (notRich && (x + y) !=1)
+        {
+            return $"{x+y} dollars";
+        }
+        else
+        {
+            return $"YOU BROKE MY FRIEND!!!!";
+        }
+
+    }
     }
 }
